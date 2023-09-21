1fa0a1c2d3c1c4767314dd4454534ab1c56d940a
Barry Hoggard <barry.hoggard@sothebys.com>
2023-09-21 16:42:28 -0400

    update path for bazel and go

--- dot_oh-my-zsh/custom/custom.zsh
+++ dot_oh-my-zsh/custom/custom.zsh
@@ -1,7 +1,10 @@
 . ~/.secrets
 
-export HOMEBREW_NO_ENV_HINTS=true
+export PATH=/Users/barry.hoggard/code/ws-monorepo:/usr/local/bin:$PATH
+export GOROOT=$HOME/code/ws-monorepo/bazel-ws-monorepo/external/go_sdk
+export GOPATH=$GOROOT
 
+export HOMEBREW_NO_ENV_HINTS=true
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 alias ch=chezmoi
