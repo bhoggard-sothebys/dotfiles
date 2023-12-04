a0720b3e8044ddf5848ef8b824a27f1141e7e804
Barry Hoggard <145390016+bhoggard-sothebys@users.noreply.github.com>
2023-12-04 14:05:58 -0500

    update zsh files

--- dot_oh-my-zsh/custom/custom.zsh
+++ dot_oh-my-zsh/custom/custom.zsh
@@ -1,13 +1,50 @@
 . ~/.secrets
 
-export PATH=/Users/barry.hoggard/code/ws-monorepo:/usr/local/bin:$PATH
-export GOROOT=$HOME/code/ws-monorepo/bazel-ws-monorepo/external/go_sdk
-export GOPATH=$GOROOT
+export GOPATH=$HOME/code/ws-monorepo/bazel-ws-monorepo/external
+export GOROOT=$GOPATH/go_sdk
+export PATH=$GOPATH/bin:/usr/local/bin:$PATH
 
 export HOMEBREW_NO_ENV_HINTS=true
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-alias ch=chezmoi
+export AWS_PROFILE=viking_dev
 
+export EDITOR=nvim
+# export VISUAL=code
 
+. /Users/barry.hoggard/code/ws-monorepo/tools/sh/k8
 
+eval "$(ssh-agent -s)"
+
+function gogenerate() {
+    bash -c "cd $1 && go generate -x" \;
+    find $1 -type d \( ! -name . \) -exec bash -c "cd '{}' && go generate -x" \;
+}
+
+aws_vikingprod() {
+    export AWS_PROFILE=viking_prod
+    export AWS_REGION=us-east-1
+    export AWS_DEFAULT_REGION=$AWS_REGION
+}
+
+aws_vikingregistrationprod() {
+    export AWS_PROFILE=viking_registrationprod
+    export AWS_REGION=us-east-1
+    export AWS_DEFAULT_REGION=$AWS_REGION
+}
+
+aws_vikingstaging() {
+    export AWS_PROFILE=viking_dev
+    export AWS_REGION=us-east-1
+    export AWS_DEFAULT_REGION=$AWS_REGION
+}
+
+aws_vikingdev() {
+    export AWS_PROFILE=viking_dev
+    export AWS_REGION=eu-west-1
+    export AWS_DEFAULT_REGION=$AWS_REGION
+}
+
+aws_none() {
+    unset AWS_PROFILE AWS_REGION AWS_DEFAULT_REGION
+}
